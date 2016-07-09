898a3de6aecd54d9daa1a90f22ccdeb2a017d30f
Mattdre <olsen_matthew@yahoo.com>
2016-07-08 15:43:28 -1000

    Receiving data from devleague, client is working!

--- client.js
+++ client.js
@@ -2,6 +2,22 @@ const net = require('net');
 const http = require('http');
 const client = new net.Socket();
 
-client.connect({port: 8080}, (()=>{
+let now = new Date().toUTCString();
+let fullURL= process.argv[2]; // URL
+let pathway_start = fullURL.indexOf('/');
+let host_name = fullURL.slice(0,pathway_start);
+let pathway = fullURL.slice(pathway_start); // URI
+
+client.connect({port: 80, host: host_name}, ()=>{
   console.log('Connected to server.');
-}));
+  client.write(
+    `GET ${pathway} HTTP/1.1\n`+
+    `Host: ${host_name}\n`+
+    `Date: ${now}\n`+
+    `Connection: Keep-Alive\n`+
+    `Accept: text/html, application/json\n\n`);
+  client.on('data', (data)=>{
+    process.stdout.write(data);
+    // client.end();
+  });
+});
--- server.js
+++ server.js
@@ -1,41 +1,47 @@
-const net = require('net');
-const fs = require('fs');
+const net = require( 'net' );
+const fs = require( 'fs' );
 
-const server = net.createServer((socket)=>{
-  socket.on('data', (data)=>{
+const server = net.createServer( ( socket ) => {
+  socket.on( 'data', ( data ) => {
 
     data = data.toString();
-    let endOfRequestURI = data.indexOf(' ', 4);
-    let requestURI = data.slice(4, endOfRequestURI);
+    console.log(data);
+    let endOfRequestURI = data.indexOf( ' ', 4 );
+    let requestURI = data.slice( 4, endOfRequestURI );
+    let now = new Date().toUTCString();
 
-    fs.readFile('.'+ requestURI, (err, data) => {
-      if(err){
-        fs.readFile('./404.html', (err, data) => {
+    fs.readFile( '.' + requestURI, ( err, data ) => {
+      if( err ) {
+        fs.readFile( './404.html', ( err, data ) => {
           let body = data.toString();
           let output =
             `HTTP/1.0 404 NotFound
-            content-length: ${body.length}
+            Date: ${now}
+            Content-Length: ${body.length}
+            Connection: keep-alive
 
             ${body}`;
-          socket.write(output);
+          socket.write( output );
           socket.end();
         });
 
-      }else{
+      } else {
         let body = data.toString();
         let output =
           `HTTP/1.1 200 OK
+          Date: ${now}
           Content-Length: ${body.length}
+          Connection: keep-alive
 
           ${body}`;
-        socket.write(output);
+        socket.write( output );
+        console.log('Client received ' + `${requestURI}` + ' on ' + `${now}`);
         socket.end();
-        // end connection and console log to server that client received data etc..
       }
     });
   });
 });
 
-server.listen('8080', () =>{
-  console.log('Server listening on port 8080.');
+server.listen( '8080', () => {
+  console.log( 'Server listening on port 8080.' );
 });
